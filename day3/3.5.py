2527fb3ad9f34f221fd5bc3ba56590d69f3fab1a
Simon <sclayton1006@outlook.com>
2021-05-11 13:26:07 +0100

    Love calculator v2 - updated 11/05/2021

--- day3/3.5.py
+++ day3/3.5.py
@@ -3,22 +3,26 @@ name1 = input("What is your name? \n").lower()
 name2 = input("What is their name? \n").lower()
 name3 = name1 + name2
 
-value = 0
+value1 = 0
+value2 = 0
 
-value += name3.count("t")
-value += name3.count("r")
-value += name3.count("u")
-value += name3.count("e")
-value += name3.count("l")
-value += name3.count("o")
-value += name3.count("v")
-value += name3.count("e")
+value1 += name3.count("t")
+value1 += name3.count("r")
+value1 += name3.count("u")
+value1 += name3.count("e")
+value2 += name3.count("l")
+value2 += name3.count("o")
+value2 += name3.count("v")
+value2 += name3.count("e")
 
-if value < 10 or value > 90:
-    print(f"Your score is {value}, you go together like \
+bounce = f"{value1}{value2}"
+value3 = int(bounce)
+
+if value3 < 10 or value3 > 90:
+    print(f"Your score is {value3}, you go together like \
 coke and mentos.")
-elif value > 40 and value < 50:
-    print(f"Your score is {value}, you are alright \
+elif value3 > 40 and value3 < 50:
+    print(f"Your score is {value3}, you are alright \
 together.")
 else:
-    print(f"Your score is {value}.")
+    print(f"Your score is {value3}.")
